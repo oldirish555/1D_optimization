a38daca07a6a075583c21dbae6b457ea3cb4a735
oldirish555 <45312064+oldirish555@users.noreply.github.com>
2024-03-12 09:52:05 +0700

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def f(x):
     return (x+4)**2+2*x # example function
 
-def brute_force(f, a, b, step_size):
+def bruteforce(f, a, b, step_size):
     x_min = None
     f_min = float('inf')
     x = a
@@ -22,7 +22,7 @@ b = 11
 x = np.linspace(a, b, 100)
 y = f(x)
 
-min_x = brute_force(f, a, b, 2)
+min_x = bruteforce(f, a, b, 2)
 min_y = f(min_x)
 
 plt.plot(x, y)
@@ -32,6 +32,6 @@ plt.scatter(min_x, min_y, color='red')
 
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Minimization of 1D Function by Brute Force Method')
+plt.title('Minimization of 1D Function by Bruteforce Method')
 
 plt.show()
